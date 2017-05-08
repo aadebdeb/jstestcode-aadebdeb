094cbd0e0c98d7fb816b6d911cca4f96c40ea1f5
aadebdeb <aa.debdeb@gmail.com>
2017-05-08 21:07:12 +0900

    first commit

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+function alertAnswer() {
+    var first = parseInt(document.getElementById('first').value);
+    var second = parseInt(document.getElementById('second').value);
+
+    alert(sum(first, second));
+}
---
+++ spec/sum-test.js
@@ -0,0 +1,11 @@
+describe('test of sum', function() {
+    it('1+1=2', function() {
+        expect(sum(1, 1)).toBe(2);
+    });
+    it('-1+1=0', function() {
+        expect(sum(-1, 1)).toBe(0);
+    });
+    it('0+1=1', function() {
+        expect(sum(0, 1)).toBe(1);
+    });
+});
